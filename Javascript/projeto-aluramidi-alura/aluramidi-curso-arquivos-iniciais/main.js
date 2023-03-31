eee4d562223fffab45f5770c95f502e1e66082ce
Mateus Souza <mattwstech@gmail.com>
2023-03-31 20:44:35 -0300

    adding the new features

--- Javascript/projeto-aluramidi-alura/aluramidi-curso-arquivos-iniciais/main.js
+++ Javascript/projeto-aluramidi-alura/aluramidi-curso-arquivos-iniciais/main.js
@@ -1,1 +1,27 @@
-alert("Olá mundo");
+function tocaSomPom(idElementoAudio) {
+  document.querySelector(idElementoAudio).play();
+}
+
+const ListaDeTeclas = document.querySelectorAll(".tecla");
+
+let contador = 0;
+
+for (let contador = 0; contador < ListaDeTeclas.length; contador++) {
+  const tecla = ListaDeTeclas[contador];
+
+  const instrumento = tecla.classList[1];
+
+  const idAudio = `#som_${instrumento}`;
+
+  tecla.onclick = function () {
+    tocaSomPom(idAudio);
+  };
+
+  tecla.onkeydown = function () {
+    ListaDeTeclas[contador].classList.add("ativa");
+  };
+
+  tecla.onkeyup = function () {
+    ListaDeTeclas[contador].classList.remove("ativa");
+  };
+}
