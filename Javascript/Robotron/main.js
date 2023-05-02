d3d9c5e1f3df127c78d5e630cc905b985b492815
Mateus Souza <mattwstech@gmail.com>
2023-05-02 00:37:36 -0300

    dataatributtes nas tags

--- Javascript/Robotron/main.js
+++ Javascript/Robotron/main.js
@@ -1,19 +1,20 @@
-const braco = document.querySelector("#braco");
 const subtrair = document.querySelector("#subtrair");
 const somar = document.querySelector("#somar");
 
-const controle = document.querySelectorAll(".controle-ajuste");
+const controle = document.querySelectorAll("[data-controle]");
 
 controle.forEach((elemento) => {
   elemento.addEventListener("click", (evento) => {
-    ManipulaDados(evento.target.textContent);
+    ManipulaDados(evento.target.dataset.controle, evento.target.parentNode);
   });
 });
 
-function ManipulaDados(operacao) {
+function ManipulaDados(operacao, controle) {
+  const peca = controle.querySelector("[data-contador]");
+
   if (operacao == "+") {
-    braco.value = parseInt(braco.value) + 1;
+    peca.value = parseInt(peca.value) + 1;
   } else {
-    braco.value = parseInt(braco.value) - 1;
+    peca.value = parseInt(peca.value) - 1;
   }
 }
